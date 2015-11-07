0cf60694428f62055ad1bc25039ed13044185c6b
Khang Dinh <thesephi@gmail.com>
2015-11-07 15:19:05 +0800

    [server] Can insert a test user and /list all users

--- core-server/server.js
+++ core-server/server.js
@@ -1,6 +1,17 @@
 "use strict";
 
 var restify = require('restify');
+var r = require('rethinkdb');
+
+var config = {
+  dbhost: 'localhost',
+  dbport: 28015,
+  dbname: 'drever'
+}
+
+/** CONSTANTS */
+var USER = 'user';
+var BOOKING = 'booking';
 
 var server = restify.createServer();
 server.use(restify.CORS());
@@ -21,6 +32,52 @@ server.get('/test', function(req, res, next) {
   res.json(200, "Hi, Drever User!");
 });
 
+/** APIS */
+server.get('/list', function(req, res, next) {
+  dbconn(function(err, conn) {
+    if(err) return res.send(err);
+    r.table(BOOKING)
+    .coerceTo('array')
+    .run(conn, function(err, result) {
+      return handleCRUDTrans(err, result, conn, res);
+    });
+  });
+});
+
+server.get('/insertTestUser', function(req, res, next) {
+  insertTestUser(res);
+});
+
 server.listen(18865, function() {
   console.log('Drver Core Server listening at %s', server.url);
 });
+
+/** HELPERS */
+
+function dbconn(cb) {
+  r.connect({ host: config.dbhost, port: config.dbport }, function(err, conn) {
+    if(err) return cb(err);
+    conn.use(config.dbname);
+    return cb(null, conn);
+  });
+}
+
+function handleCRUDTrans(err, result, conn, res) {
+  conn.close();
+  if(err)
+    return res.json(500, err);
+  return res.json(result);
+}
+
+function insertTestUser(res) {
+  dbconn(function(err, conn) {
+    if(err) return res.send(err);
+    r.table(BOOKING)
+    .insert({
+      email: 'user1@drever.cangroup.io'
+    })
+    .run(conn, function(err, result) {
+      return handleCRUDTrans(err, result, conn, res);
+    });
+  });
+}
