7e1dc6f4a2db68e97d1595f52542102a67de5f40
Khang Dinh <thesephi@gmail.com>
2015-11-08 11:22:00 +0800

    [driver] Added default info for driver

--- driver/www/js/controllers.js
+++ driver/www/js/controllers.js
@@ -6,4 +6,15 @@ angular.module('starter.controllers', [])
 
 .controller('SearchCtrl', function($scope) {
   console.log('SearchCtrl');
+  $scope.$on("$ionicView.loaded", function() {
+    // set the logged in driver
+    // so we don't have to deal with the boring sign-up, sign-in stuff
+    var driver = {
+      email: "Aditya_Franecki@geraldine.us",
+      id: "a447d902-d86e-4ef9-b74e-4f6531cf806d",
+      name: "Jayda Gibson",
+      avatar: "http://drever.codeatnite.com/uploads/driver0.jpg"
+    }
+    localStorage.setItem('dre_driver', JSON.stringify(driver));
+  });
 });
